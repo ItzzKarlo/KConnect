89929a628abf5f76446216bad8f2cf93811e67a5
unknown <karlo.cavlovic@outlook.com>
2026-06-05 13:56:59 +0200

    Added User Model

--- windows-client/KConnect/Models/User.cs
+++ windows-client/KConnect/Models/User.cs
@@ -1,6 +1,12 @@
-﻿namespace KConnect.Models;
+﻿// !/windows-client/kconnect/Models/User.cs
+namespace KConnect.Models;
 
 public class User
 {
-    
+    public Guid Id { get; set; }
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string? Phone { get; set; }
+    public bool MfaEnabled { get; set; }
+    public bool IsVerified { get; set; }
 }
