47cc928b44549c4e15e0177f731bed12ff964ecd
unknown <karlo.cavlovic@outlook.com>
2026-06-05 11:21:46 +0200

    Added Routers to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.api import auth, mfa
 
-app = FastAPI(title="KConnect API")
+app = FastAPI(title="KConnect API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
@@ -10,6 +11,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+app.include_router(auth.router)
+app.include_router(mfa.router)
 
 @app.get("/health")
 def health():
