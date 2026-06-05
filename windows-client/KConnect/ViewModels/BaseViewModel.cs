98474a130b0666043af0e1732abd0bd45ee0aa50
unknown <karlo.cavlovic@outlook.com>
2026-06-05 14:01:03 +0200

    Added BaseViewModel

--- windows-client/KConnect/ViewModels/BaseViewModel.cs
+++ windows-client/KConnect/ViewModels/BaseViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace KConnect.ViewModels;
+﻿// !/windows-client/kconnect/ViewModels/BaseViewModel.cs
+using CommunityToolkit.Mvvm.ComponentModel;
 
-public class BaseViewModel
+namespace KConnect.ViewModels;
+
+public abstract partial class BaseViewModel : ObservableObject
 {
-    
+    [ObservableProperty] private bool _isBusy;
+    [ObservableProperty] private string _errorMessage = "";
+    [ObservableProperty] private string _successMessage = "";
+
+    protected void SetError(string msg)
+    {
+        ErrorMessage = msg;
+        SuccessMessage = "";
+    }
+
+    protected void SetSuccess(string msg)
+    {
+        ErrorMessage = "";
+        SuccessMessage = msg;
+    }
+
+    protected void ClearMessages()
+    {
+        ErrorMessage = "";
+        SuccessMessage = "";
+    }
 }
